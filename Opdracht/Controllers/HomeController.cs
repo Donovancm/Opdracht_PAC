793f6413ae68712047f684f487dc6f7bea2a5aa9
Donovancm <donovan.dcm@gmail.com>
2019-09-25 13:26:25 +0200

    eerste commit

--- Opdracht/Controllers/HomeController.cs
+++ Opdracht/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -12,6 +13,43 @@ namespace Opdracht.Controllers
     {
         public IActionResult Index()
         {
+            List<int> listOfValues = new List<int>();
+            //Shortcut to the Input.csv file
+            var path = "~/../Data/Input.csv";
+            int total;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] values = line.Split(",");
+                    foreach (var item in values)
+                    {
+                        if (item == "")
+                        {
+                            listOfValues.Add(0);
+                        }
+                        else
+                        {
+                            listOfValues.Add(int.Parse(item));
+                        }
+
+                    }
+                }
+            }
+            listOfValues.RemoveRange(20, 5);
+            listOfValues.RemoveRange(40, 5);
+            listOfValues.RemoveRange(60, 5);
+
+            var value = 0;
+            total = 0;
+            for (int i = 1; i < listOfValues.Count(); i += 2)
+            {
+                value += listOfValues[i];
+                total = value;
+            }
+            ViewData["Som"] = total;
+            //ViewBag
             return View();
         }
 
